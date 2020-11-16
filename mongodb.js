e0c0d1c524037865b65f48211aacbd85a1b227de
Frank Minichiello <9028036fmini@users.noreply.github.com>
2020-11-16 09:13:54 -0700

    added tasks collection and insertMany to add 3 test tasks

--- mongodb.js
+++ mongodb.js
@@ -14,9 +14,60 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Frank",
-      age: 50,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Frank",
+    //     age: 50,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jen",
+    //       age: 28,
+    //     },
+    //     {
+    //       name: "Gunther",
+    //       age: 27,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert documents");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Wash Clothes",
+          completed: true,
+        },
+        {
+          description: "Dry Clothes",
+          completed: false,
+        },
+        {
+          description: "Eat Breakfast",
+          completed: false,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to save documents");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
