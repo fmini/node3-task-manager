efbeeec91085e2865618ee36ad099a13fcb8013d
Frank Minichiello <9028036fmini@users.noreply.github.com>
2020-11-17 15:32:11 -0700

    added required as validation to User name and custom validation for User age not allowing negative numbers

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,44 +9,50 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
   },
 });
 
-// const me = new User({
-//   name: "Frank",
-//   age: 50,
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => {
-//     console.log("Error", error);
-//   });
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-  },
+const me = new User({
+  name: "Frank",
+  age: -50,
 });
 
-const task = new Task({
-  description: "Learn to code",
-  completed: false,
-});
-
-task
-  .save()
+me.save()
   .then(() => {
-    console.log(task);
+    console.log(me);
   })
   .catch(error => {
     console.log("Error", error);
   });
+
+// const Task = mongoose.model("Task", {
+//   description: {
+//     type: String,
+//   },
+//   completed: {
+//     type: Boolean,
+//   },
+// });
+
+// const task = new Task({
+//   description: "Learn to code",
+//   completed: false,
+// });
+
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch(error => {
+//     console.log("Error", error);
+//   });
