de9c4de31fcbab81f1babf2481fd6fb3adbe9e80
Frank Minichiello <9028036fmini@users.noreply.github.com>
2020-11-19 16:05:43 -0700

    moved task model to task.js in models, created task route in index.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,28 +5,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-// const task = new Task({
-//   description: "  Lose weight ",
-// });
-
-// task
-//   .save()
-//   .then(() => {
-//     console.log(task);
-//   })
-//   .catch(error => {
-//     console.log("Error", error);
-//   });
