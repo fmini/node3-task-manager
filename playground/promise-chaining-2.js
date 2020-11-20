36e2758023e1da982ab80522e7ca113c4227a164
Frank Minichiello <9028036fmini@users.noreply.github.com>
2020-11-20 16:48:37 -0700

    reformated promise chaining to use async await in playground folder

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,12 +1,27 @@
 require("../src/db/mongoose");
+const { findByIdAndDelete } = require("../src/models/task");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("5fb6f725ed9b1b33003b33fc")
-  .then(() => {
-    return Task.countDocuments({ completed: false });
-  })
-  .then(result => {
-    console.log(result);
+// Task.findByIdAndDelete("5fb6f725ed9b1b33003b33fc")
+//   .then(() => {
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async (id, status) => {
+  await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: status });
+  return count;
+};
+
+deleteTaskAndCount("5fb851690479f427903d0c9d", false)
+  .then(count => {
+    console.log(count);
   })
   .catch(e => {
     console.log(e);
